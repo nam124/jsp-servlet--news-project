b0e1e1bfa3537132f4d80eb63f6a2e3ab55db884
nam124 <voldemort097@gmail.com>
2020-05-28 01:41:04 +0700

    test commit

--- src/main/java/com/nam317/constant/SystemConstant.java
+++ src/main/java/com/nam317/constant/SystemConstant.java
@@ -5,5 +5,5 @@ public class SystemConstant {
 	public static final String ADMIN = "ADMIN";
 	public static final String USER = "USER";
 	public static final String LIST = "list";
-	public static final String EDIT = "edit";
+	public static final String EDIT = "edit";//
 }
